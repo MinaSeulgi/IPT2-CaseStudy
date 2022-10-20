f6a62441fc95bbb54669a03b519e8decda9e3ce4
JOY <brionesjoy009@gmail.com>
2022-10-20 15:00:21 +0800

    added the method receipts

--- OrderingSystem.java
+++ OrderingSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
@@ -8,17 +9,24 @@ public class OrderingSystem {
     
     static Map<Object,String> toppings = new HashMap<>();
     static Map<Object, String> sizes = new HashMap<>();
+    static Map<Object, String> responses = new HashMap<>();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        greet();
-
-        populateSizes();
-        String userSize = askForSize();
-
-        String userToppingQuantity = askForToppingQuantity();
-    }
         
+        populateSizes();
+        populateToppings();
+        populateResponses();
+        
+        greet();
+        
+        String userSize = askForSize();
+        String userToppingQuantity = askForToppingQuantity();
+        String userToppings[] = askForToppings(Integer.parseInt(userToppingQuantity));
+        
+        receipt(userSize, userToppingQuantity, userToppings);
+    }
+    
     private static String askForToppingQuantity() {
         String quantity = "";
         boolean isValidQuantity = false;
@@ -29,59 +37,6 @@ public class OrderingSystem {
             System.out.println((!isValidQuantity ? "INVALID INPUT" : getResponse()));
         }
         return quantity;
-    }
-
-    private static void greet() {
-        System.out.println("""
-                           WELCOME TO LA LUNA PIZZA STORE!
-                           You can buy assured that we provide quality pizzas!""");
-    }
-
-    private static void populateSizes() {
-        sizes.put(1, "Small");
-        sizes.put(2, "Regular");
-        sizes.put(3, "Medium");
-        sizes.put(4, "Large");
-        sizes.put(5, "Jumbo");
-    }
-
-	 private static String askForSize() {
-        String selectedSize = "";
-        boolean isValidSize = false;
-        while (!isValidSize) {
-            System.out.println("\nTo start, choose your desired size:");
-            for(Map.Entry<Object, String> entry : sizes.entrySet()) {
-                System.out.printf("\t%s. %s\n", entry.getKey(), entry.getValue());
-            }
-            System.out.print("Enter number => ");
-            selectedSize = sc.nextLine();
-            isValidSize = validate(selectedSize, "size");
-            System.out.println((!isValidSize ? "INVALID INPUT" : getResponse()));
-        }
-        return sizes.get(Integer.parseInt(selectedSize));
-    }
-    
-    private static boolean isSizePresent(int key) {
-        return sizes.containsKey(key);
-    }
-
-    private static boolean validate(String num, String type) {
-        try {
-            boolean isPresent;
-            switch(type) {
-                case "size":
-                    isPresent = isSizePresent(Integer.parseInt(num));
-                    if (!isPresent) { return false; }
-                case "topping":
-                    isPresent = isToppingPresent(Integer.parseInt(num));
-                    if (!isPresent) { return false; }
-                case "quantity":
-                    if (Integer.parseInt(num) > toppings.size()) { return false; }
-            }
-        } catch(NumberFormatException e) {
-            return false;
-        }
-        return true;
     }
     
     private static String[] askForToppings(int quantity) {
@@ -117,9 +72,99 @@ public class OrderingSystem {
         return selectedToppings.toArray(new String[quantity]);
     }
     
+    private static String askForSize() {
+        String selectedSize = "";
+        boolean isValidSize = false;
+        while (!isValidSize) {
+            System.out.println("\nTo start, choose your desired size:");
+            for(Map.Entry<Object, String> entry : sizes.entrySet()) {
+                System.out.printf("\t%s. %s\n", entry.getKey(), entry.getValue());
+            }
+            System.out.print("Enter number => ");
+            selectedSize = sc.nextLine();
+            isValidSize = validate(selectedSize, "size");
+            System.out.println((!isValidSize ? "INVALID INPUT" : getResponse()));
+        }
+        return sizes.get(Integer.parseInt(selectedSize));
+    }
+    
+    private static void populateSizes() {
+        sizes.put(1, "Small");
+        sizes.put(2, "Regular");
+        sizes.put(3, "Medium");
+        sizes.put(4, "Large");
+        sizes.put(5, "Jumbo");
+    }
+    
+    private static void populateToppings() {
+        toppings.put(1, "Ham");
+        toppings.put(2, "Cheese");
+        toppings.put(3, "Hotdog");
+        toppings.put(4, "Pineapple");
+        toppings.put(5, "Mushroom");
+        toppings.put(6, "Red Onion");
+        toppings.put(7, "Pepperoni");
+    }
+    
     private static boolean isToppingPresent(int key) {
         return toppings.containsKey(key);
     }
     
+    private static boolean isSizePresent(int key) {
+        return sizes.containsKey(key);
+    }
 
+    private static boolean validate(String num, String type) {
+        try {
+            boolean isPresent;
+            switch(type) {
+                case "size":
+                    isPresent = isSizePresent(Integer.parseInt(num));
+                    if (!isPresent) { return false; }
+                case "topping":
+                    isPresent = isToppingPresent(Integer.parseInt(num));
+                    if (!isPresent) { return false; }
+                case "quantity":
+                    if (Integer.parseInt(num) > toppings.size()) { return false; }
+            }
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    private static String getResponse() {
+        int max = 10;
+        int min = 1;
+        int range = max - min + 1;
+        return responses.get((int)(Math.random() * range) + min);
+    }
+
+    private static void populateResponses() {
+        responses.put(1, ":::::::::: Awesome!");
+        responses.put(2, ":::::::::: Great choice!");
+        responses.put(3, ":::::::::: Nice one!");
+        responses.put(4, ":::::::::: Yeeeesss!");
+        responses.put(5, ":::::::::: That's what I'm talking about!");
+        responses.put(6, ":::::::::: Woah! Niiiiice!");
+        responses.put(7, ":::::::::: Noice!");
+        responses.put(8, ":::::::::: Superb!");
+        responses.put(9, ":::::::::: You got it!");
+        responses.put(10, ":::::::::: Gotcha!");
+    }
+
+    private static void greet() {
+        System.out.println("""
+                           WELCOME TO LA LUNA PIZZA STORE!
+                           You can buy assured that we provide quality pizzas!""");
+    }
+    
+    private static void receipt(String size, String quantity, String[] toppings) {
+        System.out.println("\n\n=== ORDER SLIP ===");
+        System.out.println("SIZE: " + size);
+        System.out.println("NO. OF TOPPINGS: " + quantity);
+        System.out.println("TOPPINGS: " + Arrays.toString(toppings));
+        System.out.println("==================");
+        System.out.println("\nTHANKS FOR BUYING!\n- La Luna Pizza Store");
+    }
 }
